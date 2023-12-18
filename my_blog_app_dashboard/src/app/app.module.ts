8348a567ee45517365a823a7d0f5ce16355cfaa6
NoviceCoderInfinity <anupamscience06@gmail.com>
2023-12-18 08:45:25 +0530

    First App Dashboard Commit

---
+++ my_blog_app_dashboard/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AngularFireModule } from '@angular/fire';
+
+import { environment } from '../environments/environment.prod';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ my_blog_app_dashboard/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD_BypGjewLUnU2bEcJ4jMPvyfuwZAtvc0',
+    authDomain: 'myblogapp-d7552.firebaseapp.com',
+    projectId: 'myblogapp-d7552',
+    storageBucket: 'myblogapp-d7552.appspot.com',
+    messagingSenderId: '334167821461',
+    appId: '1:334167821461:web:728bc92000306c3a689f9c',
+  },
+};
